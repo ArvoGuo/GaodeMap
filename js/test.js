81aee2fb8c3d8de1db58e06042be7196b6f1e3b9
Arvo <ArvoGuo@gmail.com>
2014-12-10 22:00:15 +0800

    高德地图的操作及保存

---
+++ js/test.js
@@ -0,0 +1,1 @@
+Object {map: c, zIndex: 10, strokeColor: "#321400", strokeOpacity: 0.8, strokeWeight: 2…}bubble: falseclickable: trueextData: ObjectfillColor: "#967864"fillOpacity: 0.5map: cpath: Array[4]strokeColor: "#321400"strokeDasharray: Array[2]strokeOpacity: 0.8strokeStyle: "solid"strokeWeight: 2zIndex: 10__proto__: Object
